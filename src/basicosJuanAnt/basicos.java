b2573f75891b5ae80413938540955619e0f329ac
Juan <juananguillo@gmail.com>
2019-11-25 21:09:13 +0100

    Numericos

--- src/basicosJuanAnt/basicos.java
+++ src/basicosJuanAnt/basicos.java
@@ -3,8 +3,29 @@ package basicosJuanAnt;
 public class basicos {
 
 	public static void main(String[] args) {
-		
+	
 
 	}
+	 
+	  public static void numericos() {
+
+	        //Estos son los tipos básicos
+	        byte elMasPeque;
+	        short chiquitin;
+	        int entero;
+	        long duplicaEntero;
+	        float grande;
+	        double doble;
+
+	        //Ejemplos de asignación de valores a las variables de tipos básicos
+	        entero = 10;
+	        doble = 5.6;
+	   
+	        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+	        System.err.print("Valor entero ");
+	        System.out.println(entero);
+	        System.out.print("Valor doble ");
+	        System.out.println(doble);
+	    }
 
 }
