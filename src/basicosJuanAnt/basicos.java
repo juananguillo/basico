5b6323f76fccfef0f746b17b2e1269dad8c289f2
Juan <juananguillo@gmail.com>
2019-11-26 19:48:46 +0100

    Metodo logico

--- src/basicosJuanAnt/basicos.java
+++ src/basicosJuanAnt/basicos.java
@@ -3,8 +3,22 @@ package basicosJuanAnt;
 public class basicos {
 
 	public static void main(String[] args) {
-		
+		logicos();
 
+	}
+	
+	public static void logicos() {
+		
+		boolean cierto=false;
+		
+		if (cierto==true) {
+			System.out.println("Es cierto");
+			
+		} else {
+			System.out.println("Es falso");
+			
+		}
+		
 	}
 
 }
